88626cbc7dd527154bf6d1c1b98bc8b46149d68c
prosantomazumder <prosantomazumder@gmail.com>
2020-04-04 02:03:40 +0600

    version 1.0.1

---
+++ theme.scripts.js
@@ -0,0 +1,58 @@
+(function ($) {
+    "use strict";
+
+    /**!-------------------------------------------
+        01. PRELOADER
+    --------------------------------------------**/
+    var $mainwindow = $(window);
+    $mainwindow.on('load', function() {
+
+        $("#loader").fadeOut(1000);
+    });
+    
+    /**!-------------------------------------------
+        03. SLICK SLIDER
+    --------------------------------------------**/
+    function mainSlider() {
+        var BasicSlider = $('.slider-active');
+        BasicSlider.on('init', function (e, slick) {
+            var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+            doAnimations($firstAnimatingElements);
+        });
+        BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+            var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+            doAnimations($animatingElements);
+        });
+        BasicSlider.slick({
+            autoplay: true,
+            autoplaySpeed: 10000,
+            dots: true,
+            fade: true,
+            prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-long-arrow-left"></i></button>',
+            nextArrow: '<button type="button" class="slick-next"><i class="fa fa-long-arrow-right"></i></button>',
+            arrows: true,
+            responsive: [
+                { breakpoint: 767, settings: { dots: false, arrows: false } }
+            ]
+        });
+
+        function doAnimations(elements) {
+            var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            elements.each(function () {
+                var $this = $(this);
+                var $animationDelay = $this.data('delay');
+                var $animationType = 'animated ' + $this.data('animation');
+                $this.css({
+                    'animation-delay': $animationDelay,
+                    '-webkit-animation-delay': $animationDelay
+                });
+                $this.addClass($animationType).one(animationEndEvents, function () {
+                    $this.removeClass($animationType);
+                });
+            });
+        }
+    }
+    mainSlider();
+
+    
+})(jQuery);
